157c8475644c62340126d8a4cf191618e8140b60
Hirose Tomoyuki <hrstmyk811m@gmail.com>
2021-03-23 16:35:14 +0900

    フォント描画がOKになった

--- src/fonts.rs
+++ src/fonts.rs
@@ -4,20 +4,28 @@ extern "C" {
     pub static mut fonts: [[u8; 16usize]; 256usize];
 }
 
-unsafe fn get_font(character: char) -> &'static [u8; 16usize] {
+unsafe fn get_font(character: u8) -> &'static [u8; 16usize] {
     let char_idx = character as usize;
     &fonts[char_idx]
 }
 
-pub unsafe fn write_font(x: u32, y: u32, character: char, writer: &PixelWriter) {
+pub unsafe fn write_byte(x: u32, y: u32, character: u8, writer: &PixelWriter) {
     let font_slice = get_font(character);
     for dy in 0..16 {
         for dx in 0..8 {
-            if ((font_slice[dy] << dx) & 0x80) == 1 {
+            if ((font_slice[dy] << dx) & 0x80) == 0x80 {
                 writer.write(x + dx as u32, y + dy as u32, &PixelColor::White);
             } else {
                 writer.write(x + dx as u32, y + dy as u32, &PixelColor::Black);
             }
         }
+    }
+}
+
+pub unsafe fn write_string(x: u32, y: u32, s: &str, writer: &PixelWriter) {
+    let mut dx = 0;
+    for i in 0..s.len() {
+        write_byte(x + dx, y, s.as_bytes()[i], writer);
+        dx += 8;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use core::panic::PanicInfo;
 extern crate htos;
 use htos::boot_info::BootInfo;
 use htos::graphics::{PixelWriter, PixelColor};
-use htos::fonts::write_font;
+use htos::fonts::write_string;
 
 extern "C" {
     pub static mut fonts: [[u8; 16usize]; 256usize];
@@ -17,7 +17,7 @@ extern "C" {
 pub unsafe extern "C" fn kernel_entry(binfo: *mut BootInfo) -> ! {
     let pixel_writer = PixelWriter { video_info: &mut (*binfo).vinfo };
     pixel_writer.write_background(PixelColor::Black);
-    write_font(0, 0, 'c', &pixel_writer);
+    write_string(0, 0, "hello, HTOS!", &pixel_writer);
     loop {}
 }
 

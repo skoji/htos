b251d30060ed7f88fa8a1be08cd5d89e54da114d
Hirose Tomoyuki <hrstmyk811m@gmail.com>
2021-03-25 07:53:12 +0900

    画面再描画でまだバグっている

--- src/console.rs
+++ src/console.rs
@@ -1,65 +1,80 @@
-use crate::graphics::{PixelWriter, PixelColor};
+use crate::graphics::PixelWriter;
 use crate::fonts::write_byte;
 
 // 画面サイズ 80x25
 pub const ROW_NUMBER: usize = 25;
 pub const COLUNM_NUMBER: usize = 80;
 
-pub struct Console<'a> {
+pub struct Console {
     pub position_x: usize, // 横のカーソル位置
     pub position_y: usize, // 縦のカーソル位置
-    pub screen_buffer: [u8; COLUNM_NUMBER * ROW_NUMBER], // スクリーンバッファ
-    pub data_head: usize, // バッファ上の一番最初のデータがあるインデックス
-    pub data_len: usize, // バッファにあるデータの長さ
-    pub pixel_writer: &'a PixelWriter, // 画面描画構造体
+    pub screen_buffer: [[u8; COLUNM_NUMBER]; ROW_NUMBER], // スクリーンバッファ
+    pub row_head_index: usize,    // バッファ内のデータの最初の行
+    pub row_tail_index: usize,    // バッファ内のデータの最後の行
+    pub column_tail_index: usize, // バッファ内のデータの最後の行の最後の列
+    pub pixel_writer: PixelWriter, // 画面描画構造体
 }
 
-impl<'a> Console<'a> {
-    // バッファに1文字格納するメソッド
-    fn set_screen_buffer(&mut self, c: u8) {
-        if self.data_len < COLUNM_NUMBER * ROW_NUMBER {
-            // データがバッファからあふれていない場合
-            self.screen_buffer[self.data_head + self.data_len] = c;
-            self.data_len += 1;
-        } else {
-            // データがいっぱいになった場合
-            self.screen_buffer[(self.data_head + self.data_len) % (COLUNM_NUMBER + ROW_NUMBER)] = c;
-            self.data_head = (self.data_head + 1) % (COLUNM_NUMBER + ROW_NUMBER);
+impl Console {
+    // バッファの末尾のインデックスを次の行にするメソッド
+    fn buffer_newline(&mut self) {
+        self.column_tail_index = 0;
+        self.row_tail_index = (self.row_tail_index + 1) % ROW_NUMBER;
+        if self.row_tail_index == self.row_head_index {
+            // もし既存の行を上書きする場合
+            // headをインクリメントする
+            self.row_head_index = (self.row_head_index + 1) % ROW_NUMBER;
+            // 既存の行を'\0'埋めしておく
+            for i in 0..COLUNM_NUMBER {
+                self.screen_buffer[self.row_tail_index][i] = b'\0';
+            }
         }
     }
 
-    fn newline(&mut self) {
+    // バッファに1文字格納するメソッド
+    fn push_screen_buffer(&mut self, c: u8) {
+        if c == b'\n' {
+            self.buffer_newline();
+        } else {
+            self.column_tail_index = (self.column_tail_index + 1) % COLUNM_NUMBER;
+            if self.column_tail_index == 0 {
+                self.buffer_newline();
+            }
+            self.screen_buffer[self.row_tail_index][self.column_tail_index] = c;
+        }
+    }
+
+    fn screen_newline(&mut self) {
         self.position_x = 0;
         if self.position_y + 1 < ROW_NUMBER {
             self.position_y += 1;
         } else {
-            // 画面を黒で塗りつぶす
-            for y in 0..(ROW_NUMBER * 16) {
-                for x in 0..(COLUNM_NUMBER * 8) {
-                    self.pixel_writer.write(x, y, &PixelColor::Black);
+            // 一番下に来ている場合
+            // バッファから画面を書き直す
+            for y in 0..ROW_NUMBER {
+                for x in 0..COLUNM_NUMBER {
+                    write_byte(x * 8, y * 16, self.screen_buffer[(self.row_head_index + y) % ROW_NUMBER][x], &self.pixel_writer);
                 }
             }
-            // TODO: 文字を書き直す
         }
     }
 
     fn put_char(&mut self, c: u8) {
         if c == b'\n' {
-            self.newline();
+            self.screen_newline();
         } else {
-            write_byte(self.position_x * 8, self.position_y * 16, c, self.pixel_writer);
-            if self.position_x + 1 < COLUNM_NUMBER {
-                self.position_x += 1;
-            } else {
-                self.newline();
+            write_byte(self.position_x * 8, self.position_y * 16, c, &self.pixel_writer);
+            self.position_x = (self.position_x + 1) % COLUNM_NUMBER;
+            if self.position_x == 0 {
+                self.screen_newline();
             }
         }
     }
 
     // 1文字画面に出力するメソッド
     pub fn put(&mut self, c: u8) {
-        self.set_screen_buffer(c);
-        self.put_char(self.screen_buffer[self.data_head + self.data_len - 1]);
+        self.push_screen_buffer(c);
+        self.put_char(c);
     }
 
     // 一行画面に出力する関数
--- src/main.rs
+++ src/main.rs
@@ -15,72 +15,17 @@ pub extern "C" fn kernel_entry(binfo: *mut BootInfo) -> ! {
     let mut console = Console {
         position_x: 0,
         position_y: 0,
-        screen_buffer: [b'\0'; COLUNM_NUMBER * ROW_NUMBER],
-        data_head: 0,
-        data_len: 0,
-        pixel_writer: &pixel_writer,
+        screen_buffer: [[b'\0'; COLUNM_NUMBER]; ROW_NUMBER],
+        row_head_index: 0,
+        row_tail_index: 0,
+        column_tail_index: 0,
+        pixel_writer,
     };
 
-    console.puts("00\n");
-    console.puts("01\n");
-    console.puts("02\n");
-    console.puts("03\n");
-    console.puts("04\n");
-    console.puts("05\n");
-    console.puts("06\n");
-    console.puts("07\n");
-    console.puts("08\n");
-    console.puts("09\n");
-    console.puts("10\n");
-    console.puts("11\n");
-    console.puts("12\n");
-    console.puts("13\n");
-    console.puts("14\n");
-    console.puts("15\n");
-    console.puts("16\n");
-    console.puts("17\n");
-    console.puts("18\n");
-    console.puts("19\n");
-    console.puts("20\n");
-    console.puts("21\n");
-    console.puts("22\n");
-    console.puts("23\n");
-    console.puts("24\n");
-    console.puts("25\n");
-    console.puts("26\n");
-    console.puts("27\n");
-    console.puts("28\n");
-    console.puts("29\n");
-    console.puts("30\n");
-    console.puts("31\n");
-    console.puts("32\n");
-    console.puts("33\n");
-    console.puts("34\n");
-    console.puts("35\n");
-    console.puts("36\n");
-    console.puts("37\n");
-    console.puts("38\n");
-    console.puts("39\n");
-    console.puts("40\n");
-    console.puts("41\n");
-    console.puts("42\n");
-    console.puts("43\n");
-    console.puts("44\n");
-    console.puts("45\n");
-    console.puts("46\n");
-    console.puts("47\n");
-    console.puts("48\n");
-    console.puts("49\n");
-    console.puts("50\n");
-    console.puts("51\n");
-    console.puts("52\n");
-    console.puts("53\n");
-    console.puts("54\n");
-    console.puts("55\n");
-    console.puts("56\n");
-    console.puts("57\n");
-    console.puts("58\n");
-    console.puts("59\n");
+    for _ in 0..(ROW_NUMBER - 1 + 2) {
+        console.put(b'0');
+        console.put(b'\n');
+    }
 
     loop {}
 }
